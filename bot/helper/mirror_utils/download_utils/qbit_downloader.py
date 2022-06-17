e99eda95dfc3598aec6a13be9bfb8cc3b929f6b9
PiyushMudela <47444569+PiyushMudela@users.noreply.github.com>
2022-06-17 11:05:12 +0530

    Update qbit_downloader.py

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -4,18 +4,17 @@ from re import search as re_search
 from telegram import InlineKeyboardMarkup
 from telegram.ext import CallbackQueryHandler
 
-from bot import download_dict, download_dict_lock, BASE_URL, dispatcher, get_client, STOP_DUPLICATE, WEB_PINCODE, QB_SEED, TORRENT_TIMEOUT, LOGGER
+from bot import download_dict, download_dict_lock, BASE_URL, dispatcher, get_client, TORRENT_DIRECT_LIMIT, ZIP_UNZIP_LIMIT, STOP_DUPLICATE, WEB_PINCODE, QB_SEED, TORRENT_TIMEOUT, LOGGER, STORAGE_THRESHOLD
 from bot.helper.mirror_utils.status_utils.qbit_download_status import QbDownloadStatus
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, deleteMessage, sendStatusMessage, update_all_messages
-from bot.helper.ext_utils.bot_utils import getDownloadByGid, get_readable_time, setInterval
-from bot.helper.ext_utils.fs_utils import clean_unwanted, get_base_name
+from bot.helper.ext_utils.bot_utils import getDownloadByGid, get_readable_file_size, get_readable_time, setInterval
+from bot.helper.ext_utils.fs_utils import clean_unwanted, get_base_name, check_storage_threshold
 from bot.helper.telegram_helper import button_build
 
 
 class QbDownloader:
     POLLING_INTERVAL = 3
-
     def __init__(self, listener):
         self.__listener = listener
         self.__path = ''
@@ -26,7 +25,8 @@ class QbDownloader:
         self.ext_hash = ''
         self.__stalled_time = time()
         self.__uploaded = False
-        self.is_seeding = False
+        self.__seeding = False
+        self.__sizeChecked = False
         self.__dupChecked = False
         self.__rechecked = False
 
@@ -99,7 +99,6 @@ class QbDownloader:
         except Exception as e:
             sendMessage(str(e), self.__listener.bot, self.__listener.message)
             self.client.auth_log_out()
-
     def __qb_listener(self):
         try:
             tor_info = self.client.torrents_info(torrent_hashes=self.ext_hash)
@@ -130,6 +129,29 @@ class QbDownloader:
                             self.__onDownloadError("File/Folder is already available in Drive.")
                             sendMarkup("Here are the search results:", self.__listener.bot, self.__listener.message, button)
                     self.__dupChecked = True
+                if not self.__sizeChecked:
+                    size = tor_info.size
+                    arch = any([self.__listener.isZip, self.__listener.extract])
+                    if STORAGE_THRESHOLD is not None:
+                        acpt = check_storage_threshold(size, arch)
+                        if not acpt:
+                            msg = f'You must leave {STORAGE_THRESHOLD}GB free storage.'
+                            msg += f'\nYour File/Folder size is {get_readable_file_size(size)}'
+                            self.__onDownloadError(msg)
+                            return
+                    limit = None
+                    if ZIP_UNZIP_LIMIT is not None and arch:
+                        mssg = f'Zip/Unzip limit is {ZIP_UNZIP_LIMIT}GB'
+                        limit = ZIP_UNZIP_LIMIT
+                    elif TORRENT_DIRECT_LIMIT is not None:
+                        mssg = f'Torrent limit is {TORRENT_DIRECT_LIMIT}GB'
+                        limit = TORRENT_DIRECT_LIMIT
+                    if limit is not None:
+                        LOGGER.info('Checking File/Folder Size...')
+                        if size > limit * 1024**3:
+                            fmsg = f"{mssg}.\nYour File/Folder size is {get_readable_file_size(size)}"
+                            self.__onDownloadError(fmsg)
+                    self.__sizeChecked = True
             elif tor_info.state == "stalledDL":
                 if not self.__rechecked and 0.99989999999999999 < tor_info.progress < 1:
                     msg = f"Force recheck - Name: {self.__name} Hash: "
@@ -160,7 +182,7 @@ class QbDownloader:
                             self.periodic.cancel()
                             return
                         download_dict[self.__listener.uid] = QbDownloadStatus(self.__listener, self)
-                    self.is_seeding = True
+                    self.__seeding = True
                     update_all_messages()
                     LOGGER.info(f"Seeding started: {self.__name}")
                 else:
@@ -174,7 +196,6 @@ class QbDownloader:
                 self.periodic.cancel()
         except Exception as e:
             LOGGER.error(str(e))
-
     def __onDownloadError(self, err):
         LOGGER.info(f"Cancelling Download: {self.__name}")
         self.client.torrents_pause(torrent_hashes=self.ext_hash)
@@ -183,19 +204,17 @@ class QbDownloader:
         self.client.torrents_delete(torrent_hashes=self.ext_hash, delete_files=True)
         self.client.auth_log_out()
         self.periodic.cancel()
-
     def cancel_download(self):
-        if self.is_seeding:
+        if self.__seeding:
             LOGGER.info(f"Cancelling Seed: {self.__name}")
             self.client.torrents_pause(torrent_hashes=self.ext_hash)
         else:
             self.__onDownloadError('Download stopped by user!')
-
 def get_confirm(update, context):
     query = update.callback_query
     user_id = query.from_user.id
     data = query.data
-    data = data.split()
+    data = data.split(" ")
     qbdl = getDownloadByGid(data[2])
     if not qbdl:
         query.answer(text="This task has been cancelled!", show_alert=True)
@@ -209,6 +228,5 @@ def get_confirm(update, context):
         qbdl.client().torrents_resume(torrent_hashes=data[3])
         sendStatusMessage(qbdl.listener().message, qbdl.listener().bot)
         query.message.delete()
-
 qbs_handler = CallbackQueryHandler(get_confirm, pattern="qbs", run_async=True)
 dispatcher.add_handler(qbs_handler)
